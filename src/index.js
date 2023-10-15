eb4c0d4c228b9b8acd195b1779716397d62e2abc
iradbouzidi <b.irad1994@gmail.com>
2023-10-15 14:53:14 +0100

    deploy

--- src/index.js
+++ src/index.js
@@ -6,3 +6,4 @@ import App from "./App";
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // https://www.youtube.com/watch?v=377AQ0y6LPA
+// https://github1s.com/adrianhajdin/project_e_commerce
